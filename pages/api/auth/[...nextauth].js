6939eefd30fc719a10c08d1b6a1684daa623488d
leehj04271 <122263151+leehj04271@users.noreply.github.com>
2023-06-09 15:42:46 +0900

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,59 +15,6 @@ export const authOptions = {
         // ...add more providers here
     ],
 
-    callbacks: {
-   /*      async signIn({user}) {
-            const client = await getClient();
 
-            const sogae = client.db("sogae");
-
-            const usersCollection = sogae.collection("users");
-
-            const existingUser = await usersCollection.findOne({
-                                                                   email: user.email,
-                                                               });
-
-            if (!existingUser) {
-                await sogae
-                    .collection("users")
-                    .insertOne({email: user.email});
-                await client.close();
-
-
-            } else {
-                await client.close();
-                //return '/signup'
-
-            }
-
-            return true;
-        }, */
-
-        async session({session, token, user}) {
-            // Send properties to the client, like an access_token and user id from a provider.
-            session.accessToken = token.accessToken
-            session.user.id = token.id
-            session.user.nickname = 'nicknamee'
-
-            const client = await getClient();
-
-            const sogae = client.db("sogae");
-
-            const usersCollection = sogae.collection("users");
-
-            console.log(session.user.email)
-            const existingUser = await usersCollection.findOne({
-                                                                   email: session.user.email,
-                                                               });
-            await  client.close();
-
-            console.log(existingUser)
-
-            session.user.profile = existingUser.profile
-
-            return session
-        }
-    },
-    secret: process.env.NEXTAUTH_SECRET,
 };
 export default NextAuth(authOptions);
