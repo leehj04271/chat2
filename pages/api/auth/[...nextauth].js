32feb09fc7f2923970cd6939359b7f4c67d098bd
leehj04271 <122263151+leehj04271@users.noreply.github.com>
2023-06-09 16:54:51 +0900

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import dotenv from "dotenv";
-import {getClient} from "@/lib/db";
+import { getClient } from "@/lib/db";
+import { redirect } from "next/dist/server/api-utils";
 
 dotenv.config();
 
@@ -9,26 +10,20 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
-                           clientId: process.env.GOOGLE_CLIENT_ID,
-                           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                       }),
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
         // ...add more providers here
     ],
- 
-      callbacks: {
-    async signIn({ account, profile }) {
-      if (account.provider === "google") {
-        return profile.email_verified && profile.email.endsWith("@example.com")
-      }
-      return true // Do different verification for other providers that don't have `email_verified`
+
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account.provider === "google") {
+              return profile.email_verified && profile.email.endsWith("@example.com")
+            }
+            return true // Do different verification for other providers that don't have `email_verified`
+          },
     },
-  }
-    
-
-      
-    },
-
-
     secret: process.env.NEXTAUTH_SECRET,
 };
 export default NextAuth(authOptions);
