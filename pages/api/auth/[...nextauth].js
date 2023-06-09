5451ebde8b805984d74946dc94695c2d21271a65
leehj04271 <122263151+leehj04271@users.noreply.github.com>
2023-06-09 16:57:44 +0900

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,7 @@ export const authOptions = {
     callbacks: {
         async signIn({ account, profile }) {
             if (account.provider === "google") {
-              return profile.email_verified && profile.email.endsWith("@example.com")
+              return profile.email_verified 
             }
             return true // Do different verification for other providers that don't have `email_verified`
           },
